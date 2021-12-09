e010543606e9c67a2a72bd8db4e61a96f7eef88e
Scott Webber <swebber@hwpnj.com>
2021-12-08 21:11:46 -0500

    Day 8 Part 1

---
+++ Day8a/Day8a/Program.cs
@@ -0,0 +1,19 @@
+﻿int simpleCount = 0;
+int[] digits = new int[] { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+string fileName = @"C:\Users\WebberS\source\repos\AdventOfCode2021\Day8a\Day8a\day8-data.txt";
+
+var displayLines = File.ReadAllLines(fileName);
+
+foreach (var displayLine in displayLines)
+{
+    var segments = displayLine.Split(" | ");
+    var signalPatterns = segments[0].Split(" ").ToList();
+    var outputValues = segments[1].Split(" ").ToList();
+
+    simpleCount += 
+        outputValues
+            .Select(outputValue => digits.Count(digit => digit == outputValue.Length))
+            .Count(count => count == 1);
+}
+
+Console.WriteLine($"How many times to digits 1, 4, 7, or 8 appear? {simpleCount}");
