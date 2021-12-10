cd2d9d4ca8cd90155a1c09bdc92d98b29aba1fba
Scott Webber <swebber@hwpnj.com>
2021-12-09 21:25:07 -0500

    Day 9 Solution to Part 2

--- Day9/Day9/Program.cs
+++ Day9/Day9/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using System.Drawing;
 
 string fileName = @"C:\Users\WebberS\source\repos\AdventOfCode2021\Day9\Day9\day9-data.txt";
 
 var depthData = new List<int[]>();
+var basins = new List<Point>();
+var basinSizes = new List<int>();
 
 var lines = File.ReadLines(fileName);
 foreach (var line in lines)
@@ -10,20 +12,86 @@ foreach (var line in lines)
     depthData.Add(line.Select(s => int.Parse(s.ToString())).ToArray());
 }
 
+int count;
 int dataHeight = depthData.Count;
 int dataWidth = depthData[0].Length;
 
-var total = 0;
 for (int row = 0; row < depthData.Count; row++)
 {
     for (int col = 0; col < depthData[row].Length; col++)
     {
-        var depth = GetDepth(row, col);
-        if (IsPointLow(row, col)) total += depth + 1;
+        if (IsPointLow(row, col)) basins.Add(new Point(col, row));
     }
 }
 
-Console.WriteLine($"Sum of risk from low points: {total}");
+var basinPoints = new List<Point>();
+
+foreach (var basin in basins)
+{
+    count = 0;
+    basinPoints.Clear();
+    WalkBasin(basin);
+    basinSizes.Add(basinPoints.Count);
+}
+
+int max = 1;
+
+for (int i = 0; i < 3; i++)
+{
+    // https://stackoverflow.com/a/1143552
+    int index =
+        basinSizes
+            .Select((value, index) => new { Value = value, Index = index })
+            .Aggregate((a, b) => (a.Value > b.Value) ? a : b)
+            .Index;
+
+    // or since i have no idea what the above does...
+    index = basinSizes.IndexOf(basinSizes.Max());
+
+    max *= basinSizes[index];
+    basinSizes.RemoveAt(index);
+}
+
+Console.WriteLine($"Product of three largest basins: {max}");
+
+void WalkBasin(Point location)
+{
+    if (basinPoints.Contains(location)) return;
+
+    // How much do I trust my recursive code?
+    if (++count > 1000) throw new Exception();
+
+    int depth = GetDepthAtPoint(location);
+    if (depth < 9) basinPoints.Add(location);
+
+    int row = location.Y;
+    int col = location.X;
+
+    // look right - row, col + 1
+    var point = new Point(col + 1, row);
+    depth = GetDepthAtPoint(point);
+    if (depth < 9) WalkBasin(point);
+
+    // look down - row + 1, col
+    point = new Point(col, row + 1);
+    depth = GetDepthAtPoint(point);
+    if (depth < 9) WalkBasin(point);
+
+    // look left - row, col - 1
+    point = new Point(col - 1, row);
+    depth = GetDepthAtPoint(point);
+    if (depth < 9) WalkBasin(point);
+
+    // look up - row - 1, col
+    point = new Point(col, row - 1);
+    depth = GetDepthAtPoint(point);
+    if (depth < 9) WalkBasin(point);
+}
+
+int GetDepthAtPoint(Point point)
+{
+    return GetDepth(point.Y, point.X);
+}
 
 int GetDepth(int row, int col)
 {
