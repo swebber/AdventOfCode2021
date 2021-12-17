60b86120031885308b870976c8ad7a62ec587126
Scott Webber <swebber@hwpnj.com>
2021-12-16 21:58:48 -0500

    Day 15 Part 2

--- Day15/Day15/Program.cs
+++ Day15/Day15/Program.cs
@@ -6,6 +6,9 @@ InitHeightAndWidth();
 
 var field = new Point[height,width];
 LoadField();
+ExtendField();
+
+//DumpField();
 
 bool updated = true;
 while (updated)
@@ -14,6 +17,70 @@ while (updated)
 }
 
 Console.WriteLine(field[height - 1, width - 1].Depth);
+
+void DumpField()
+{
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            Console.Write(field[row, col].Value);
+        }
+        Console.WriteLine();
+    }
+}
+
+void ExtendField()
+{
+    int efWidth = width / 5;
+    int efHeight = height / 5;
+
+    for (int x = 1; x < 5; x++)
+    {
+        int row1 = 0;
+        int row2 = efHeight;
+        int col1 = efWidth * (x - 1);
+        int col2 = col1 + efWidth;
+        int rightOffset = efWidth;
+        ExtendRight(row1, col1, row2, col2, rightOffset);
+    }
+
+    for (int x = 1; x < 5; x++)
+    {
+        int row1 = efHeight * (x - 1);
+        int row2 = row1 + efHeight;
+        int col1 = 0;
+        int col2 = width;
+        int downOffset = efHeight;
+        ExtendDown(row1, col1, row2, col2, downOffset);
+    }
+}
+
+void ExtendRight(int row1, int col1, int row2, int col2, int offset)
+{
+    for (int row = row1; row < row2; row++)
+    {
+        for (int col = col1; col < col2; col++)
+        {
+            int value = field[row, col].Value + 1;
+            if (value > 9) value = 1;
+            field[row, col + offset] = new Point(value, long.MaxValue);
+        }
+    }
+}
+
+void ExtendDown(int row1, int col1, int row2, int col2, int offset)
+{
+    for (int row = row1; row < row2; row++)
+    {
+        for (int col = col1; col < col2; col++)
+        {
+            int value = field[row, col].Value + 1;
+            if (value > 9) value = 1;
+            field[row + offset, col] = new Point(value, long.MaxValue);
+        }
+    }
+}
 
 bool WalkField()
 {
@@ -117,6 +184,9 @@ void InitHeightAndWidth()
         ++height;
         if (line.Length > width) width = line.Length;
     }
+
+    height *= 5;
+    width *= 5;
 }
 
 internal class Point
